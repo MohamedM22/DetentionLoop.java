252e8a7a770911ebd46a042e710d1efb17b4ebae
Mohamed Moulaye <109633435+MohamedM22@users.noreply.github.com>
2023-02-09 16:48:34 -0500

    Add files via upload

---
+++ Detention.java
@@ -0,0 +1,14 @@
+public class Detention {
+    public static void main(String[] args) {
+                
+     
+
+        System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?");
+
+
+        for (int i=0; i<99; i++) {
+            System.out.println(i+1 + ". I will not forget semi-colons.");
+    
+    }
+}
+}
